6a011524ad26df9468f2111072e9df9b0fb829b8
Paul <paul.wiench2@gmail.com>
2025-10-14 15:50:17 +0200

    Plot 3d top view of the first building in fetched test data.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import json
 import requests
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import numpy as np
+
 
 url = "https://overpass-api.de/api/interpreter"
 
@@ -18,3 +22,15 @@ for element in building_data["elements"]:
     for lat_long_dict in element["geometry"]:
         geometry.append((lat_long_dict['lat'], lat_long_dict['lon']))
     building_geometries.append(geometry)
+
+geom = np.array(building_geometries[0])
+x, y = geom[:, 0], geom[:, 1]
+
+bottom = np.column_stack((x, y, np.zeros_like(x)))
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.add_collection3d(Poly3DCollection([bottom], color='gray'))
+
+plt.show()

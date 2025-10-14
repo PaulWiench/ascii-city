75a2c8b15353f3b9b12e07a441639c471d555fe7
Paul <paul.wiench2@gmail.com>
2025-10-14 15:24:03 +0200

    Read payload from query file.

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import requests
 
 url = "https://overpass-api.de/api/interpreter"
 
-payload = """
-[out:json];
-(
-  way["building"](around:100,48.748297,9.104774);
-);
-out geom;
-"""
+payload = r''
+with open('buildings.overpassql', 'r') as query:
+    payload += (query.read().replace('\n', ''))
 
 r = requests.post(url, data=payload)
 

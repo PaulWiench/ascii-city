21ee1926b5a155834af1ddff2f1df66e3b503ffd
Paul <paul.wiench2@gmail.com>
2025-10-18 19:50:38 +0200

    Define cache getter function.

--- src/db/caching.py
+++ src/db/caching.py
@@ -1,5 +1,14 @@
 import boto3
+import json
 
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("ascii_city_cache")
+
+def get_cache(
+        key: str
+) -> dict | None:
+    response = table.get_item(Key={"cache_key": key})
+
+    if "Item" in response:
+        return json.loads(response["Item"]["value"])
